0f2460aedddbda6334ef124db6aeb85a7bae9ce0
aooXu <yichao.xu.19@ucl.ac.uk>
2020-03-11 21:43:42 +0000

    Update 25: Start to integrate the server

--- Android/app/src/main/java/com/uk/ac/ucl/carefulai/Model.java
+++
@@ -1,9 +0,0 @@
-package com.uk.ac.ucl.carefulai;
-
-public class Model {
-
-    public int calculateScore(int stepCount, int callCount, int textCount) {
-
-        return stepCount / 1000 + (callCount + textCount) / 5;
-    }
-}
