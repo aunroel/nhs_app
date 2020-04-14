08e2774dcc2841f686b292f4a937829c17364a71
VoytechG <wojtek@Wojtek-Book.localdomain>
2020-04-14 17:58:33 +0200

    manage routes into subfodlers

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ import datetime
 app = Flask(__name__)
 app.config.from_object(Config)
 config = app.config
-# csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
@@ -29,7 +28,7 @@ login.login_view = 'login'
 api = Api(app)
 bootstrap = Bootstrap(app)
 
-from auth.main import login_required
+# from auth.main import login_required
 from nhs_app.models.user_model import User
 from nhs_app.forms.user_forms import UserLogin, UserRegister
 from nhs_app.resource.project import Dashboard, Homepage, ApiDoc
@@ -37,6 +36,9 @@ from nhs_app.resource.ml_resource import MLDownload, MLTrainingResource, ModelAv
 from nhs_app.resource.user import UserLogout
 from nhs_app.resource.node import NodeRegister
 from nhs_app.resource.update_aggregator import Aggregator
+from nhs_app.resource.auth import auth
+
+app.register_blueprint(auth, url_prefix = '/api/auth')
 
 api.add_resource(Homepage, '/index', '/', endpoint='index')
 api.add_resource(Aggregator, '/update/<string:uid>', endpoint='update')
@@ -47,113 +49,54 @@ api.add_resource(Dashboard, '/dashboard', endpoint='dashboard')
 api.add_resource(MLDownload, '/model', endpoint='model')
 api.add_resource(MLTrainingResource, '/train', endpoint='train')
 api.add_resource(ModelAvailability, '/available', endpoint='available')
+# api.add_resource()
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def logino():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+
+#     form = UserLogin()
+#     if form.validate_on_submit():
+#         user = User.find_by_username(form.username.data)
+#         if user is None \
+#             or not user.check_password(form.password.data):
+#             # or user.user_type != 1:
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#         if not next_page or url_parse(next_page).netloc != '':
+#             next_page = url_for('index')
+
+#         return redirect(next_page)
+
+#     return render_template('login.html', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def logino():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = UserLogin()
-    if form.validate_on_submit():
-        user = User.find_by_username(form.username.data)
-        if user is None \
-            or not user.check_password(form.password.data):
-            # or user.user_type != 1:
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-
-        return redirect(next_page)
-
-    return render_template('login.html', form=form)
 
 
-@app.route('/api/auth/login', methods=["POST"])
-@use_kwargs({
-    "email": fields.Email(required=True),
-    "password": fields.Str(required=True), 
-})
-def login(email, password):
 
-    try :
-        user = User.find_by_email(email)
-
-        if user is None or not user.check_password(password):
-            raise ValidationError('User or password are incorrect')
-
-        encoded_jwt = user.encode_auth_token()
-
-        return jsonify({'token': encoded_jwt})
-
-    except ValidationError as e:
-        print(str(e))
-        return jsonify( { 'error' : str(e) } )
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def registero():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
+# @app.route('/register', methods=['GET', 'POST'])
+# def registero():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('dashboard'))
     
-    form = UserRegister()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, user_type=False,
-                    password=form.password.data)
-        user.save_to_db()
-        flash('Congratulations, registration completed. System admin will review and approve your permissions shortly.')
-        return redirect(url_for('index'))
+#     form = UserRegister()
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data, user_type=False,
+#                     password=form.password.data)
+#         user.save_to_db()
+#         flash('Congratulations, registration completed. System admin will review and approve your permissions shortly.')
+#         return redirect(url_for('index'))
 
-    return render_template('register.html', form=form)
-
-
-@app.route('/api/auth/test', methods=["POST"])
-@login_required
-def test(userData):
-    userId = userData['id']
-    return str(userId)
+#     return render_template('register.html', form=form)
 
 
-@app.route('/api/auth/register', methods=['POST'])
-@use_kwargs({
-    "username": fields.Str(required=True), 
-    "email": fields.Email(required=True),
-    "password": fields.Str(required=True), 
-    "password2": fields.Str(required=True), 
-    })
-def register(username, email, password, password2):
-    try: 
-        if User.find_by_email(email):
-            raise ValidationError('Email already in use. Please use different email address')
 
-        if User.find_by_username(username):
-            raise ValidationError('Username already exists. Please choose another')
 
-        if password != password2:
-            raise ValidationError("Password and Repeated Password must match")
-        
-        user = User(username = username, 
-                    email = email, 
-                    password = password,
-                    user_type = False)
 
-        user.save_to_db()
-
-        encoded_jwt = user.encode_auth_token()
-
-        return jsonify({'token': encoded_jwt})
-         
-        
-    except ValidationError as e:
-        return jsonify( { 'error' : str(e) } )
-
-    except Exception as e:
-        print(str(e))
-        return jsonify( { 'error' : "An error occurred saving the user to the database " } ), 500
 
 
 
