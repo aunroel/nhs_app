1ffd13252e0f75776dde56fe1879942538721389
VoytechG <wojtek@Wojtek-Book.localdomain>
2020-05-06 04:39:52 +0200

    fixes

---
+++ static/precache-manifest.8e3422123576eb6764e95b1f96844ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07e1e24d5ee0dfbf7643dfda4929c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b0842d58ad301b59d7",
+    "url": "/static/css/main.02ccb58b.chunk.css"
+  },
+  {
+    "revision": "b8377f4d1c5c8023eec5",
+    "url": "/static/js/2.b4e6ff32.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.b4e6ff32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b0842d58ad301b59d7",
+    "url": "/static/js/main.41c810d6.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
